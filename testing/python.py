830809c8c75943e279a7717cb5b3ef01223d71b0
Tardanoir <tardanoir@gmail.com>
2025-10-23 22:33:41 -0300

    test files

---
+++ testing/python.py
@@ -0,0 +1,17 @@
+import os
+
+'''a string'''
+
+class Foo(object):
+    def __init__(self, name):
+        os.path.join('foo', 'bar')
+        num = 42
+        print(num)
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        self._name = value
